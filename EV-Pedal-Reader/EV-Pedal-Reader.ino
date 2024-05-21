f145c7cd657829785f1180bcebc8b1dcea599767
TheAlphaDogs4946 <humberviewrobotics@gmail.comgit config --global user.email humberviewrobotics@gmail.com>
2024-05-21 10:40:25 -0400

    added the arduino code for ease of access

---
+++ EV-Pedal-Reader/EV-Pedal-Reader.ino
@@ -0,0 +1,13 @@
+int prevValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int raw = analogRead(A0);
+  if (prevValue != raw) {
+    Serial.println(raw);
+    prevValue = raw;  
+  }
+}
